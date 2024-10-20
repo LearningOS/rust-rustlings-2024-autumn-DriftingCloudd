820485033d48748869b48a4dad82c78dfe8bbb08
DriftingCloud <driftingcloudd@outlook.com>
2024-10-20 14:58:19 +0800

    Try clippy1

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -12,9 +12,10 @@
 
 
 use std::f32;
+use std::f32::consts::PI;
 
 fn main() {
-    let pi = std::f32::consts::PI;
+    let pi = PI;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
